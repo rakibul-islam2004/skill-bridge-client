43086feb2c6ff21603848201828ef6c846357c92
rakibul-islam2004 <mdrakibulislamandrokib@gmail.com>
2026-02-04 21:27:29 +0600

    fix(build): resolve Invalid URL error and next.config warnings

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,19 +1,27 @@
 import { createAuthClient } from "better-auth/react";
 
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") {
+    return window.location.origin;
+  }
+  return (
+    process.env.NEXT_PUBLIC_BETTER_AUTH_URL ||
+    "https://skill-bridge-client-five.vercel.app"
+  ); // Build side
+};
+
 export const authClient = createAuthClient({
-  baseURL: "/", 
+  baseURL: getBaseUrl(),
   basePath: "/api/v1/auth",
   fetchOptions: {
     credentials: "include",
   },
 });
 
-// 1. Export the User type so it can be imported in other files
 export type User = typeof authClient.$Infer.Session.user & {
   role?: "STUDENT" | "TUTOR" | "ADMIN";
 };
 
-// 2. Export the Session type using the User type above
 export type Session = typeof authClient.$Infer.Session & {
   user: User;
 };
